5981bd727206d5ee1e0274c725bc31920099c886
oihugu <53376595+oihugu@users.noreply.github.com>
2020-06-09 23:20:37 -0300

    mudanças visuais

--- project/main.py
+++ project/main.py
@@ -4,13 +4,17 @@ sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 import pygame
 import project
-
+from pygame.locals import *
 #
 #
 #Importante o comando sys vai servir para podermos chamar todos os modulos dentro da pasta projects
 #
 #
-janela = project.menu.tela.iniciar_pygame()
+path = './project/art'
+os.chdir(path)
+pygame.init()
+pygame.display.set_caption("Jogo da Velha")
+janela = project.menu.tela.iniciar_pygame((720,480))
 continuar = True
 pagina_atual = project.menu
 
@@ -18,6 +22,10 @@ while continuar:
         for event in pygame.event.get(): #pega os eventos
             if event.type == pygame.QUIT: #sair do jogo, esta aqui pois funciona em todas as telas
                 continuar = False
+
+            elif event.type == pygame.VIDEORESIZE:
+                novo_tamanho = event.size
+                project.menu.tela.iniciar_pygame(novo_tamanho)
 
             elif event.type == pygame.KEYDOWN:
                 if event.key in pagina_atual.controles.definidos.keys(): #Procura a key na classe controles da pagina atual
