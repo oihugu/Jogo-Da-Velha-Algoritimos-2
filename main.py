318c1650af14fd4d1e9f4bee7d15ce7187516cc6
oihugu <53376595+oihugu@users.noreply.github.com>
2020-06-14 15:52:47 -0300

    adicionando main e remanejando o código do menu

---
+++ main.py
@@ -0,0 +1,43 @@
+import pygame
+import compartilhados
+import ranking
+import jogo
+import menu
+from pygame.locals import RESIZABLE
+
+
+altura, largura = 1200,500
+
+pygame.init()
+pygame.display.set_caption("Jogo da Velha")
+janela = pygame.display.set_mode((altura, largura))
+menu.tela_menu(janela)
+
+User = compartilhados.User
+continuar = True
+
+
+while continuar:
+    for event in pygame.event.get(): #pega os eventos
+        if event.type == pygame.QUIT:
+            continuar = False
+
+        elif event.type == pygame.VIDEORESIZE:
+            altura, largura = event.size
+            #compartilhados.tela_vazia(janela)
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                pagina_atual = ranking
+                User.na_pagina_menu = False
+                User.na_pagina_ranking = True
+                ranking.main(janela)
+            if event.key == pygame.K_j:
+                pagina_atual = jogo
+                User.na_pagina_menu = False
+                User.na_pagina_jogo = True
+                jogo.main(janela)
+
+                    
+        
+        pygame.display.flip()
--- menu.py
+++ menu.py
@@ -1,35 +1,43 @@
 import pygame
 import compartilhados
 import ranking
+import jogo
 from pygame.locals import RESIZABLE
 
-
-altura, largura = 1200,500
-
-pygame.init()
-pygame.display.set_caption("Jogo da Velha")
-janela = pygame.display.set_mode((altura, largura))
-compartilhados.tela_vazia(janela)
-
-User = compartilhados.User
-continuar = True
-
-
-while continuar:
-    for event in pygame.event.get(): #pega os eventos
-        if event.type == pygame.QUIT:
-            continuar = False
-
-        elif event.type == pygame.VIDEORESIZE:
-            altura, largura = event.size
-            compartilhados.tela_vazia(janela)
-
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_r:
-                pagina_atual = ranking
-                User.na_pagina_menu = False
-                User.na_pagina_ranking = True
-                ranking.main(janela)
-                    
-        
-        pygame.display.flip()
+def tela_menu(janela):
+    compartilhados.tela_vazia(janela)
+    BLACK = (0, 0, 0)
+    ##menu
+    menu = pygame.draw.rect(janela, BLACK, (320, 60, 550, 350), 3)
+    ##título
+    largura, altura = pygame.display.get_surface().get_size()
+    base = (largura + altura)//2
+    font = pygame.font.Font('visual/kashima.ttf', base//14)
+    text = font.render("Jogo da Velha", True, (0, 0, 0))
+    text_width, text_height = font.size("Jogo da Velha")
+    janela.blit(text,
+        (largura//15 * 6 - text_height,altura//10 + text_height//2))
+    ##opção - jogar
+    largura, altura = pygame.display.get_surface().get_size()
+    base = (largura + altura)//2
+    font = pygame.font.Font('visual/kashima.ttf', base//14)
+    text = font.render("Jogar", True, (0, 0, 0))
+    text_width, text_height = font.size("Jogar")
+    janela.blit(text,
+        (largura//12.3 *6 - text_height,altura//5 + text_height//1))
+    ##opção - creditos
+    largura, altura = pygame.display.get_surface().get_size()
+    base = (largura + altura)//2
+    font = pygame.font.Font('visual/kashima.ttf', base//14)
+    text = font.render("Creditos", True, (0, 0, 0))
+    text_width, text_height = font.size("Creditos")
+    janela.blit(text,
+        (largura//13.2 *6 - text_height,altura//2 + text_height//1))
+    ##opção - ranking
+    largura, altura = pygame.display.get_surface().get_size()
+    base = (largura + altura)//2
+    font = pygame.font.Font('visual/kashima.ttf', base//14)
+    text = font.render("Ranking", True, (0, 0, 0))
+    text_width, text_height = font.size("Ranking")
+    janela.blit(text,
+        (largura//13 *6 - text_height,altura//2.9 + text_height//1))
